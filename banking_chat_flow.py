b966fa213ec31c01a37221c437ff50015219e5a7
LPA-283284 <lpaislington@hotmail.com>
2025-06-15 00:52:14 +0100

    Update banking_chat_flow.py

--- banking_chat_flow.py
+++ banking_chat_flow.py
@@ -36,7 +36,6 @@ petty_cash = st.number_input("Petty Cash (£)", min_value=0.0, format="%.2f", va
 deposit_plus = st.number_input("Deposit ( + ) (£)", min_value=0.0, format="%.2f", value=None, placeholder="0.00", key="deposit_plus")
 tips_credit_card = st.number_input("Tips (CC) (£)", min_value=0.0, format="%.2f", value=None, placeholder="0.00", key="tips_credit_card")
 tips_sc = st.number_input("Servis Charge (£)", min_value=0.0, format="%.2f", value=None, placeholder="0.00", key="tips_sc")
-till_balance = st.number_input("Till Balance (£)", min_value=0.0, format="%.2f", value=None, placeholder="0.00", key="till_balance")
 cash_envelope = st.number_input("Cash in Envelope (£)", min_value=0.0, format="%.2f", value=None, placeholder="0.00", key="cash_envelope")
 float_val = st.number_input("Float (£)", min_value=75.00, format="%.2f", value=None, placeholder="0.00", key="float_val")
 
