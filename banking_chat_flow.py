ffaa8483732007ccf7a088a54cdeec2882bf16e2
LPA-283284 <lpaislington@hotmail.com>
2025-06-15 01:19:35 +0100

    Update banking_chat_flow.py

--- banking_chat_flow.py
+++ banking_chat_flow.py
@@ -67,13 +67,18 @@ client = gspread.authorize(credentials)
 sheet = client.open("La Petite Banking Extended").sheet1
 
 
-    if st.button("Submit"):
+    if st.button("Verileri Gönder"):
     row = [str(date), gross_total, net_total, service_charge, discount_total, complimentary_total,
            staff_food, calculated_taken_in, cc1, cc2, cc3, amex1, amex2, amex3, voucher,
            deposit_plus, deposit_minus, deliveroo, ubereats, petty_cash, tips_credit_card,
            tips_sc, calculated_till_balance, cash_envelope, float_val,
            item_missing_kitchen, item_missing_floor, eat_out,
            comments, manager, floor_staff, kitchen_staff]
+
+    sheet.append_row(row)
+    st.success("Veriler Google Sheets'e başarıyla gönderildi!")
+    st.experimental_rerun()
+
 
     sheet.append_row(row)
     st.success("Data successfully sent it!")
