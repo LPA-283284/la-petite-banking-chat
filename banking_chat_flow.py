7a82f10f0e2eeca6ae839c742fa3bdd58ae48b0d
LPA-283284 <lpaislington@hotmail.com>
2025-06-16 16:14:05 +0100

    Update banking_chat_flow.py

--- banking_chat_flow.py
+++ banking_chat_flow.py
@@ -36,8 +36,8 @@ deliveroo = st.number_input("Deliveroo (£)", min_value=0.0, format="%.2f", valu
 ubereats = st.number_input("Uber Eats (£)", min_value=0.0, format="%.2f", value=None, placeholder="0.00", key="ubereats")
 petty_cash = st.number_input("Petty Cash (£)", min_value=0.0, format="%.2f", value=None, placeholder="0.00", key="petty_cash")
 deposit_plus = st.number_input("Deposit ( + ) (£)", min_value=0.0, format="%.2f", value=None, placeholder="0.00", key="deposit_plus")
-tips_credit_card = st.number_input("Tips (CC) (£)", min_value=0.0, format="%.2f", value=None, placeholder="0.00", key="tips_credit_card")
-tips_sc = st.number_input("Servis Charge (£)", min_value=0.0, format="%.2f", value=None, placeholder="0.00", key="tips_sc")
+tips_sc = st.number_input("Servis Charge (£)", min_value=0.0, format="%.2f", value=None, placeholder="0.00", key="tips_credit_card")
+tips_credit_card = st.number_input("Tips (CC) (£)", min_value=0.0, format="%.2f", value=None, placeholder="0.00", key="tips_sc")
 
 # Çıkarılacaklar
 deducted_items = (
