af6b96f147d828dca8396289acf3c924a78e6b00
LPA-283284 <lpaislington@hotmail.com>
2025-06-16 15:30:54 +0100

    Update banking_chat_flow.py

--- banking_chat_flow.py
+++ banking_chat_flow.py
@@ -46,7 +46,7 @@ deposit_minus = st.number_input("Deposit ( - ) (£)", min_value=0.0, format="%.2
 deliveroo = st.number_input("Deliveroo (£)", min_value=0.0, format="%.2f", value=0.0, placeholder="0.00", key="deliveroo")
 ubereats = st.number_input("Uber Eats (£)", min_value=0.0, format="%.2f", value=0.0, placeholder="0.00", key="ubereats")
 petty_cash = st.number_input("Petty Cash (£)", min_value=0.0, format="%.2f", value=0.0, placeholder="0.00", key="petty_cash")
-deposit_plus = st.number_input("Deposit ( + ) (£)", min_value=0.0, format="%.2f", value=0.0e, placeholder="0.00", key="deposit_plus")
+deposit_plus = st.number_input("Deposit ( + ) (£)", min_value=0.0, format="%.2f", value=0.0, placeholder="0.00", key="deposit_plus")
 tips_credit_card = st.number_input("Tips (CC) (£)", min_value=0.0, format="%.2f", value=0.0, placeholder="0.00", key="tips_credit_card")
 tips_sc = st.number_input("Service Charge (Tips) (£)", min_value=0.0, format="%.2f", value=0.0, placeholder="0.00", key="tips_sc")
 cash_envelope = st.number_input("Cash in Envelope (£)", min_value=0.0, format="%.2f", value=0.0, placeholder="0.00", key="cash_envelope")
