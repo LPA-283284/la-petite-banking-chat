7cc7c5ad213c7afd7e144238ff43a3b5b785839c
LPA-283284 <lpaislington@hotmail.com>
2025-08-15 15:15:57 +0100

    Update banking_chat_flow.py

--- banking_chat_flow.py
+++ banking_chat_flow.py
@@ -100,7 +100,6 @@ uploaded_files = st.file_uploader("📷 Upload Receipts or Photos", type=["jpg",
 with st.form("banking_form"):
     petty_cash_note = st.text_area("Petty Cash Note")
     deposit_details = st.text_area("Deposit Details Name Date In/Out")
-    comments = st.text_area("Notes")
     manager = st.text_input("Manager")
     submitted = st.form_submit_button("Submit")
 
@@ -172,7 +171,6 @@ if submitted:
         (tips_credit_card or 0.0) + (tips_sc or 0.0) + (cash_tips or 0.0),  # CC+SC+CASH
         (float_val or 0.0),                   # Float
         petty_cash_note,                      # Petty Cash - Notes
-        notes,                                # Notes
         manager                               # Managers
     ] + images                                # IMAGES -1 to IMAGES -6
 
